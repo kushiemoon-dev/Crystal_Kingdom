3836a72663e8766f2b7d6739a9c351b887150552
Kushie <paul.margueritte@icloud.com>
2025-01-21 03:22:29 +0100

    #11 lifebar

--- player/player.gd
+++ player/player.gd
@@ -2,6 +2,11 @@ extends CharacterBody2D
 
 @export var speed: int = 100
 @onready var animations = $AnimationPlayer
+@onready var hurtColor = $Sprite2D/ColorRect
+
+@export var maxHealth = 3
+@onready var currentHealth: int = maxHealth
+
 
 func HandleInput():
 	var moveDirection = Input.get_vector( "ui_left", "ui_right", "ui_up", "ui_down" )
@@ -23,7 +28,7 @@ func handleCollision():
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
 		var collider = collision.get_collider()
-		print_debug(collider.name)
+		
 	
 	
 func _physics_process(delta):
@@ -35,4 +40,7 @@ func _physics_process(delta):
 
 func _on_hurt_box_area_entered(area: Area2D) -> void:
 	if area.name == "hitBox":
-		print_debug(area.get_parent() .name)
+		currentHealth -= 1
+		if currentHealth < 0:
+			currentHealth = maxHealth
+		
