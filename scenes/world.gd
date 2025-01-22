60b32f50845ac7d28c9c56b3417269ab180c611e
Kushie <paul.margueritte@icloud.com>
2025-01-22 19:39:00 +0100

    #16 inventory + small system for it

--- scenes/world.gd
+++ scenes/world.gd
@@ -13,3 +13,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_inventory_gui_closed() -> void:
+	get_tree().paused = false
+
+
+func _on_inventory_gui_opened() -> void:
+	get_tree().paused = true
