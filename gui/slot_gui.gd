901ade7ade82d779322cddedf1b6d18ba948c2ca
Kushie <paul.margueritte@icloud.com>
2025-01-25 18:00:02 +0100

    #19 Trying to assign value of type 'GDScript' to a variable of type 'itemStackGui.gd
    Need to be fix but dont find solution actually

--- gui/slot_gui.gd
+++ gui/slot_gui.gd
@@ -1,17 +1,11 @@
-extends Panel
+extends Button
 
 @onready var backgroundSprite: Sprite2D = $background
-@onready var itemSprite: Sprite2D = $CenterContainer/Panel/Sprite2D
-@onready var amountLabel: Label = $CenterContainer/Panel/Label
+@onready var container : CenterContainer = $CenterContainer
 
-func update(slot: InventorySlot):
-	if !slot.item:
-		backgroundSprite.frame = 0
-		itemSprite.visible = false
-		amountLabel.visible = false
-	else:
-		backgroundSprite.frame = 1
-		itemSprite.visible = true
-		itemSprite.texture = slot.item.texture
-		amountLabel.visible = slot.amount > 1
-		amountLabel.text = str(slot.amount)
+var itemStackGui = ItemStackGui
+
+func instert(isg: ItemStackGui):
+	itemStackGui = isg
+	backgroundSprite.frame = 1
+	container.add_child(itemStackGui)
